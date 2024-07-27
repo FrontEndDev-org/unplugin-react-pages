82257184a2c9df841be507cdb457813f67e691ed
#云淡然 <hi@ydr.me>
2024-07-27 19:26:03 +0800

    ci: 完善 eslint 配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,26 +2,34 @@
  * eslint.config.mjs
  * @ref https://eslint.org/
  */
-import { defineFlatConfig } from 'eslint-define-config';
-import stylistic from '@stylistic/eslint-plugin';
-import prettier from 'eslint-config-prettier';
 
-export default defineFlatConfig([
-  {
-    ignores: [
-      //
-      '**/node_modules/**',
-      '**/dist/**',
-      '**/dist-*/**',
-    ],
-    files: [
-      //
-      '**/.*.{js,cjs,mjs}',
-      '**/*.{js,mjs,cjs,ts,mts,jsx,tsx}',
-    ],
-  },
-  stylistic.configs.customize({
+import antfu from '@antfu/eslint-config';
+import imp from 'eslint-plugin-import-x';
+
+export default antfu({
+  type: 'lib',
+  ignores: ['**/node_modules/**', '**/dist/**', '**/dist-*/**'],
+  files: ['**/.*.{js,cjs,mjs}', '**/*.{js,mjs,cjs,ts,mts,jsx,tsx}'],
+  stylistic: {
     semi: true,
-  }),
-  prettier,
-]);
+  },
+  typescript: {
+    overrides: {
+      'ts/explicit-function-return-type': 'off',
+    },
+  },
+  react: true,
+  unocss: true,
+  rules: {
+    ...imp.configs.recommended.rules,
+    ...imp.configs.typescript.rules,
+    // @ref https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-unresolved.md
+    'import/no-unresolved': ['error', { ignore: ['^virtual:'] }],
+  },
+  settings: {
+    // @ref https://github.com/un-ts/eslint-plugin-import-x?tab=readme-ov-file#typescript
+    'import-x/resolver': {
+      typescript: true,
+    },
+  },
+});
