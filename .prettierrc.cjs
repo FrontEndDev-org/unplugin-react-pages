d12d776881effc66aa47d1b0a43c5b5a46a1dd99
#云淡然 <hi@ydr.me>
2024-07-27 15:23:20 +0800

    ci: eslint 升级到 9

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -4,7 +4,7 @@
  * @type {import('prettier').Config}
  */
 module.exports = {
-    tabWidth: 2,
-    singleQuote: true,
-    printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 120,
 };
---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import { defineFlatConfig } from 'eslint-define-config';
+import stylistic from '@stylistic/eslint-plugin';
+import prettier from 'eslint-config-prettier';
+
+export default defineFlatConfig([
+  {
+    ignores: [
+      //
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/dist-*/**',
+    ],
+    files: [
+      //
+      '**/.*.{js,cjs,mjs}',
+      '**/*.{js,mjs,cjs,ts,mts,jsx,tsx}',
+    ],
+  },
+  stylistic.configs.customize({
+    semi: true,
+  }),
+  prettier,
+]);
