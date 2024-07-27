05df0ca2bda68110983ca586a283f350b4d75481
#云淡然 <hi@ydr.me>
2024-07-27 13:32:35 +0800

    chore: 初始化 Monorepo

---
+++ .commitlintrc.cjs
@@ -0,0 +1,8 @@
+/**
+ * commitlint.config
+ * @ref http://commitlint.js.org/
+ * @type {import('@commitlint/types').UserConfig}
+ */
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+};
