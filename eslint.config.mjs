05c893db16e8268731a40fd6ce0071b71f19cde4
#云淡然 <hi@ydr.me>
2024-07-27 20:48:17 +0800

    ci: 优化 eslint 配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@
  * @ref https://eslint.org/
  */
 
-import antfu from '@antfu/eslint-config';
+import { antfu } from '@antfu/eslint-config';
 import imp from 'eslint-plugin-import-x';
 
 export default antfu({
@@ -15,7 +15,9 @@ export default antfu({
   },
   typescript: {
     overrides: {
-      'ts/explicit-function-return-type': 'off',
+      // 不必要显式返回类型
+      // @ref https://typescript-eslint.io/rules/explicit-function-return-type/
+      'ts/explicit-function-return-type': ['off'],
     },
   },
   react: true,
@@ -23,10 +25,15 @@ export default antfu({
   rules: {
     ...imp.configs.recommended.rules,
     ...imp.configs.typescript.rules,
+    // 忽略 virtual: 开头的 import
     // @ref https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-unresolved.md
     'import/no-unresolved': ['error', { ignore: ['^virtual:'] }],
+    // 不必要求如何使用 process
+    // @ref https://github.com/eslint-community/eslint-plugin-n/blob/HEAD/docs/rules/prefer-global/process.md
+    'node/prefer-global/process': ['off'],
   },
   settings: {
+    // 支持在 typeScript 解析 import
     // @ref https://github.com/un-ts/eslint-plugin-import-x?tab=readme-ov-file#typescript
     'import-x/resolver': {
       typescript: true,
